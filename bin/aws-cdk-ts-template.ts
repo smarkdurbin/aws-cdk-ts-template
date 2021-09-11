7e3a68c932eb9027e23b8aa04f52ac9bec334c3a
smarkdurbin <smark.durbin@gmail.com>
2021-09-11 14:46:21 -0400

    Updated aws-cdk-ts-template.ts file to use config attributes for env attribute.

--- bin/aws-cdk-ts-template.ts
+++ bin/aws-cdk-ts-template.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { AwsCdkTsTemplateStack } from '../lib/aws-cdk-ts-template-stack';
+import config from '../config/config';
 
 const app = new cdk.App();
 new AwsCdkTsTemplateStack(app, 'AwsCdkTsTemplateStack', {
@@ -15,7 +16,7 @@ new AwsCdkTsTemplateStack(app, 'AwsCdkTsTemplateStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: config.ACCOUNT, region: config.REGION },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
